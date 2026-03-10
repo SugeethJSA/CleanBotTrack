a9e8984f49703e05bf445e8b29f574650c600dac
Ridhima-Thakur07 <ridhithakur407@gmail.com>
2026-03-10 18:22:49 +0530

    Handle None distance case in decide function

--- backend/ai_model.py
+++ backend/ai_model.py
@@ -2,11 +2,14 @@ import random
 
 def decide(distance):
 
+    if distance is None:
+        return None
+
     if distance < 20:
-        return random.choice(["L", "R"])
+        return random.choice(["L","R"])
 
     elif distance < 35:
         return "F"
 
     else:
-        return random.choice(["F", "F", "L"])
+        return random.choice(["F","F","L"])
